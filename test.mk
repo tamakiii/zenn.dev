e9470e0b4116b12a956ad0d1ad9af41886263783
tamakiii <tamakiii@users.noreply.github.com>
2023-02-23 15:18:52 +0900

    add instal, build and test rule to test.mk

--- test.mk
+++ test.mk
@@ -1,10 +1,19 @@
-.PHONY: help setup clean
+.PHONY: help setup install build test clean
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 setup: \
 	test/dependency
+
+install: | dependency/zenn-dev/zenn-editor/packages/zenn-cli dependency/zenn-dev/zenn-editor/packages/zenn-markdown-html
+	$(foreach directory,$|,cd && pnpm $@)
+
+build: | dependency/zenn-dev/zenn-editor/packages/zenn-markdown-html
+	cd $| && pnpm $@
+
+test:
+	npx --no -- jest
 
 test/dependency: | dependency
 	ln -s ../$| $@
