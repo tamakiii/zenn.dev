4cdc8dfedcb927c4c3d796f0d2b299a86fc9a5ac
tamakiii <tamakiii@users.noreply.github.com>
2023-02-23 16:31:40 +0900

    rm build and clean

--- test.mk
+++ test.mk
@@ -1,4 +1,4 @@
-.PHONY: help setup install build test clean teardown
+.PHONY: help setup install test teardown
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -11,21 +11,11 @@ install:
 	cd dependency/zenn-dev/zenn-editor/packages/zenn-markdown-html && pnpm install
 	cd --declaration --allowJs --emitDeclarationOnly && pnpm build
 
-build:
-	npx --no -- tsc \
-		--declaration \
-		--emitDeclarationOnly \
-		--outDir test/types/zenn-model \
-		dependency/zenn-dev/zenn-editor/packages/zenn-model/src/*.ts
-
 test:
 	npx --no -- jest
 
 test/dependency: | dependency
 	ln -s ../$| $@
-
-clean:
-	rm -rf dependency/zenn-dev/zenn-model
 
 teardown:
 	rm -rf test/dependency
