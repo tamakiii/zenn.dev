67d7de2147b7eb1d5c936c04088cfb868f0b7029
tamakiii <tamakiii@users.noreply.github.com>
2023-02-23 14:15:18 +0900

    init jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: [
+    '<rootDir>/test'
+  ],
+  testMatch: [
+    '**/*.test.ts'
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+}
