661e4574d688215fd09a0926d8dfe78394ba0c6c
tamakiii <tamakiii@users.noreply.github.com>
2023-02-23 13:18:49 +0900

    add list rules

--- article.mk
+++ article.mk
@@ -1,16 +1,23 @@
-.PHONY: help setup
+.PHONY: help setup list
 
 TYPES := tech idea
 TYPE := $(firstword $(TYPES))
 EMOJI := 🧸
 PUBLISHED := false
 MACHINE_READABLE := false
+FORMATS := json tsv
+FORMAT := $(firstword $(FORMATS))
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 setup:
 	test -d articles
+
+list:
+	[ $(FORMAT) == "json" ] \
+		&& npx --no -- zenn list:articles --format=$(FORMAT) | jq 2> /dev/null \
+		|| npx --no -- zenn list:articles --format=$(FORMAT)
 
 articles/%.md: | articles
 	npx --no -- zenn new:article \
--- book.mk
+++ book.mk
@@ -1,15 +1,22 @@
-.PHONY: help setup
+.PHONY: help setup list
 
 PUBLISHED := false
 MACHINE_READABLE := false
 SUMMARY :=
 PRICE := 0
+FORMATS := json tsv
+FORMAT := $(firstword $(FORMATS))
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 setup:
 	test -d books
+
+list:
+	[ $(FORMAT) == "json" ] \
+		&& npx --no -- zenn list:books --format=$(FORMAT) | jq 2> /dev/null \
+		|| npx --no -- zenn list:books --format=$(FORMAT)
 
 books/%.md: | books
 	npx --no -- zenn new:book \
