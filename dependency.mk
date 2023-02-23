0e312f1935d1f2e186de2c1579b0500f969df9c1
tamakiii <tamakiii@users.noreply.github.com>
2023-02-23 14:57:53 +0900

    init dependency.mk

---
+++ dependency.mk
@@ -0,0 +1,26 @@
+.PHONY: help isntall rev-parse clean
+
+VERSION_ZENN_EDITOR :=  0.1.140
+
+help:
+	@cat $(firstword $(MAKEFILE_LIST))
+
+install: \
+	dependency/zenn-dev/zenn-editor
+
+rev-parse: | dependency/zenn-dev/zenn-editor
+	$(foreach directory,$|,git -C $(directory) rev-parse @)
+	$(foreach directory,$|,git -C $(directory) rev-parse --abbrev-ref @)
+
+dependency/zenn-dev/zenn-editor: dependency/zenn-dev
+	git clone https://github.com/zenn-dev/zenn-editor.git $@
+	git -C $@ switch -c tags/0.1.140
+
+dependency/zenn-dev: dependency
+	mkdir $@
+
+dependency:
+	mkdir $@
+
+clean:
+	rm -rf dependency
